fae469dbbb91181d4530ecfd312d572e64652eba
Samuel Matheson <samuelmatheson15@gmail.com>
2019-11-23 16:17:37 -0500

    Bash and batch command files

---
+++ Cordova (Mobile version)/Settup_Cordova.bat
@@ -0,0 +1,6 @@
+@echo off
+echo "Running Cordova setup command (npm i cordova)"
+npm i cordova
+SET /A choice=0
+echo "Cordova settup should now be completed"
+pause
---
+++ Electron (Desktop version)/settup_electron.bat
@@ -0,0 +1,5 @@
+@echo off
+echo "Running Electron setup command (npm i electron)"
+npm i electron
+echo "electron setup should now be completed"
+pause
