593d7f4e5da8d6601bd72adec9a74e23c99ee1ef
Samuel A. Matheson <57382365+anthonym01@users.noreply.github.com>
2020-06-22 16:06:13 -0500

    added server.js

---
+++ Server/Server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer(function(req,res){///make server
+    // res = response, req = request
+
+    //What the webpage will expect ot receive (similar to csp)
+    res.setHeader('Content-type','application/json')
+    res.setHeader('Acess-Control-Allow-Origin','*')
+
+});
+
+server.listen(1999, function(){//Listen to a port
+    console.log('Listening on port 1999');
+})
