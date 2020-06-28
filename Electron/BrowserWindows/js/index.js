eed17e829f54d9a4319d2bc7f514e65e825d101e
Samuel A. Matheson <57382365+anthonym01@users.noreply.github.com>
2020-06-27 22:23:51 -0500

    aptimized electron template and fixed bug

--- Electron/BrowserWindows/js/index.js
+++ Electron/BrowserWindows/js/index.js
@@ -1,186 +1,257 @@
 const main = require('electron').remote.require('./main');//acess export functions in main
-const { dialog } = require('electron').remote;//Initalize dialogue dependency
-const fs = require('fs');//Initalize file system
-const fse = require('fs-extra');//Initalize file system extra
+const { dialog, shell, Menu, MenuItem } = require('electron').remote;//Acess to electron dependencies
+const fs = require('fs');//file system
 
 window.addEventListener('load', function () {//window loads
-    if (typeof (require) == 'undefined') {//initialize node modules
-        console.error('Running in Browser')
-    } else {
-        console.log('Running in Node')
-    }
+    window_menu()
+    //textboxmenu()
     config.initialize()
-});
+})
 
-let config = {
+let config = {//Application configuration object
     properties: {
         //holdover from cordova
     },
-    baseconfig: {
+    baseconfig: {//base configuration
         use_alt_storage: false,
         alt_location: "",
     },
-    data: {
+    data: {//application data
         key: "APPnamecfg",
-        usecount: 0,
+        /*usecount: 0,*/
     },
     initialize: function () {
         console.warn('Config handler is initalized')
-        if (localStorage.getItem("APPname_cfg")) {
+        if (localStorage.getItem("APPnamecfg")) {//check if storage has the item
             this.load()
         } else {
             this.validate()
         }
     },
     save: async function () {//Save the config file
-        console.warn('Configuration is being saved')
+        console.table('Configuration is being saved', config.data)
+
+        ToStorageAPI();//save to application storage reguardless incase the file gets removed by the user, because users are kinda dumb
         if (config.baseconfig.use_alt_storage == true) {//save to alternate storage location
-            fse.ensureDirSync(config.baseconfig.alt_location.toString())//endure the directory exists
-            fs.writeFile(config.baseconfig.alt_location.toString() + "/APPnamecfgconfig.json", JSON.stringify(config.data), (err) => {//write to file
+            ToFileSystem();
+        }
+
+        function ToFileSystem() {//save config to directory defined by the user
+            console.log('saving to File system: ', config.baseconfig.alt_location.toString())
+            fs.writeFile(config.baseconfig.alt_location.toString() + "/APPnamecfg config.json", JSON.stringify(config.data), 'utf8', (err) => {//write to file
                 if (err) {//error
-                    alert("An error occurred creating the file " + err.message)
+                    alert("An error occurred creating the file, please select a new location to save app data " + err.message)
+                    config.selectlocation();
                 } else {//sucessfull
-                    console.log('config saved to: ' + config.baseconfig.alt_location.toString())
+                    console.log('config saved to: ', config.baseconfig.alt_location.toString())
                 }
             })
         }
-        console.log('config saved to application storage')
-        localStorage.setItem("APPname_cfg", JSON.stringify(config.data))//save to application storage reguardless incase the file gets removed by the user
-        console.table(config.data)
+
+        function ToStorageAPI() {//Html5 storage API
+            console.log('config saved to application storage')
+            localStorage.setItem("APPnamecfg", JSON.stringify(config.data))
+        }
     },
     load: function () {//Load the config file
         console.warn('Configuration is being loaded')
 
-        if (localStorage.getItem("APPname_cfg_baseconfig")) {//load base config firt
-            config.baseconfig = JSON.parse(localStorage.getItem("APPname_cfg_baseconfig"))
+        if (localStorage.getItem("APPnamecfg_baseconfig")) {//load base config
+            config.baseconfig = JSON.parse(localStorage.getItem("APPnamecfg_baseconfig"))
         } else {
             //first startup
-            localStorage.setItem("APPname_cfg_baseconfig", JSON.stringify(config.baseconfig))
+            localStorage.setItem("APPnamecfg_baseconfig", JSON.stringify(config.baseconfig))
         }
 
-        if (config.baseconfig.use_alt_storage == true) {
+        if (config.baseconfig.use_alt_storage == true) {//Load from alt location
             //load from alternate storage location
-            if (fs.existsSync(config.baseconfig.alt_location.toString() + "/APPname_cfgconfig.json")) {//Directory exists
-                var fileout = fs.readFileSync(config.baseconfig.alt_location.toString() + "/APPnamecfgconfig.json", { encoding: 'utf8' })
+            if (fs.existsSync(config.baseconfig.alt_location.toString() + "/APPnamecfg config.json")) {//Directory exists
+                var fileout = fs.readFileSync(config.baseconfig.alt_location.toString() + "/APPnamecfg config.json", { encoding: 'utf8' })//Read from file with charset utf8
                 console.warn('config Loaded from: ', config.baseconfig.alt_location.toString(), 'Data from fs read operation: ', fileout)
-                fileout = JSON.parse(fileout)
+                fileout = JSON.parse(fileout)//parse the json
                 if (fileout.key == "APPnamecfg") {//check if file has key
                     config.data = fileout;
                     console.warn('configuration applied from file')
-                } else {
+                } else {//no key, not correct file, load from application storage
                     console.warn('The file is not a config file, internal configuration will be used')
-                    config.data = JSON.parse(localStorage.getItem("APPname_cfg"))
+                    config.data = JSON.parse(localStorage.getItem("APPnamecfg"))
                 }
             } else {//file does not exist, was moved, deleted or is inaccesible
-                config.data = JSON.parse(localStorage.getItem("APPname_cfg"))
-                this.save()//save to recreate the file
+                config.data = JSON.parse(localStorage.getItem("APPnamecfg"))
+                alert("file does not exist, was moved, deleted or is otherwise inaccesible, please select a new location to save app data ")
+                config.selectlocation();
             }
         } else {//load from application storage
-            config.data = JSON.parse(localStorage.getItem("APPname_cfg"))
+            config.data = JSON.parse(localStorage.getItem("APPnamecfg"))
             console.log('config Loaded from application storage')
         }
 
         console.table(config.data)
         this.validate()
     },
-    validate: function () {//validate configuration file
+    validate: function () {//validate configuration
         console.warn('Config is being validated')
         var configisvalid = true
-        if (typeof (config.data.usecount) == 'undefined') {
+
+        /*if (typeof (config.data.usecount) == 'undefined') {
             config.data.usecount = 1
             configisvalid = false
             console.log('"usecount" did not exist and was set to default')
-        }
+        }*/
 
         if (!configisvalid) {
             console.log('config was found to be invalid and will be overwritten')
             this.save()//Save new confog because old config is no longer valid
         } else { console.log('config was found to be valid') }
+
     },
-    delete: function () {//Does not delete the file itself. Just sets it to empty
-        localStorage.clear("APPname_cfg")
-        config.usedefault();
+    delete: function () {//Wjipe stowage
+        localStorage.clear("APPnamecfg")//yeet storage key
+        config.usedefault();//use default location
         console.log('config deleted: ')
         console.table(config.data)
         this.validate()
     },
     backup: async function () {//backup configuration to a file
         console.warn('Configuration backup initiated')
-        var date = new Date();
-        var filepath = dialog.showSaveDialog({ /*this path must be changed*/defaultPath: "APPname_cfg backup " + Number(date.getMonth() + 1) + " - " + date.getDay() + " - " + date.getFullYear() + ".json", buttonLabel: null });
-        if (filepath == undefined) {//the file save dialogue was canceled my the user
-            console.warn('The file dialogue was canceled by the user')
-        } else {
-            fs.writeFile(filepath, JSON.stringify(config.data), (err) => {
-                if (err) {
-                    alert("An error occurred creating the file " + err.message)
-                } else {
-                    console.log("The file has been successfully saved");
-                }
-            })
-        }
-    },
-    restore: function () {//restore configuration from a file
-        console.warn('Configuration restoration initiated')
-        var filepath = dialog.showOpenDialog({ buttonLabel: "open" })
-        if (filepath == undefined) {
-            console.log("No file selected");
-        } else {
-            fs.readFile(filepath[0], 'utf-8', (err, data) => {
-                if (err) { alert("An error ocurred reading the file :" + err.message) }
-                console.log("The file content is : " + data);
-                var fileout = JSON.parse(data)
-                if (fileout.key == "APPname_cfg") {//check if this file is a timetable backup file
-                    config.data = fileout
-                    config.save();
-                    setTimeout(() => { location.reload() }, 2000)
-                } else {
-                    console.warn(filepath[0] + 'This is not a backup file')
-                }
 
-            })
-        }
-    },
-    selectlocation: function () {//select location for configuration storage
-        if (config.baseconfig.alt_location != undefined) {
-            var path = dialog.showOpenDialogSync({ properties: ['createDirectory', 'openDirectory'], defaultPath: config.baseconfig.alt_location.toString() })
-        } else {
-            var path = dialog.showOpenDialogSync({ properties: ['createDirectory', 'openDirectory'], defaultPath: null })
-        }
-        console.warn('Alternate configuration path :', path[0])
-        config.baseconfig.use_alt_storage = true
-        config.baseconfig.alt_location = path[0]
-        localStorage.setItem("APPname_cfg_baseconfig", JSON.stringify(config.baseconfig))//save base config
-        fse.ensureFileSync(config.baseconfig.alt_location.toString() + "/APPname_cfgconfig.json")//ensure the file exists, if any
-        var fileout = fs.readFileSync(config.baseconfig.alt_location.toString() + "/APPname_cfgconfig.json", { encoding: 'utf8' });//will cast error if no file, this is fine fs will catch and deal with it automatically
-        fileout = JSON.parse(fileout)
-        setTimeout(() => {
-            if (fileout != undefined) {
-                if (fileout.key == "APPname_cfg") {
-                    //a file exist here, load it
-                    console.warn('A file exists here, prompt the user on what to keep, default is currently whats in the file')
-                    config.data = fileout
-                    config.save()
-                } else {
-                    console.warn('No file exists here, config from this app is used')
-                    config.save()//to create the file
-                }
+        var date = new Date();
+        var filepath = dialog.showSaveDialog({//electron file save dialogue
+            defaultPath: "APPnamecfg backup " + Number(date.getMonth() + 1) + " - " + date.getDay() + " - " + date.getFullYear() + ".json",
+            buttonLabel: "Save"
+        });
+
+        await filepath.then((filepath) => {//resolve filepath promise
+            console.log(filepath)
+            if (filepath.canceled == true) {//the file save dialogue was canceled my the user
+                console.warn('The file dialogue was canceled by the user')
             } else {
-                console.warn('No file exists here, config from this app is used')
-                config.save()//to create the file
+                fs.writeFile(filepath.filePath, JSON.stringify(config.data), 'utf8', (err) => {//write config to file as json
+                    if (err) {
+                        alert("An error occurred creating the file " + err.message)
+                    } else {
+                        console.log("The file has been successfully saved to: ", filepath.filePath);
+                    }
+                })
             }
-        }, 500);
+        }).catch((err) => {//catch error
+            alert('An error occured ', err.message);
+        })
+    },
+    restore: async function () {//restore configuration from a file
+        console.warn('Configuration restoration initiated')
+        dialog.showOpenDialog({
+            buttonLabel: "open", filters: [
+                { name: 'Custom File Type', extensions: ['json'] },
+                { name: 'All Files', extensions: ['*'] }
+            ]
+        }).then((filepath) => {
+            console.log(filepath)
+            if (filepath.canceled == true) {//diologue ccanceled
+                console.log("diologue ccanceled");
+            } else {
+                fs.readFile(filepath.filePaths[0], 'utf-8', (err, data) => {//load data from file
+                    if (err) {
+                        alert("An error ocurred reading the file :" + err.message)
+                    } else {
+                        console.log("The file content is : " + data);
+                        var fileout = JSON.parse(data)
+                        if (fileout.key == "APPnamecfg") {//check if this file is a timetable backup file
+                            config.data = fileout
+                            config.save();
+                            setTimeout(() => {
+                                alert('Application will restart');
+                                location.reload()
+                            }, 2000)
+                        } else {
+                            console.warn(filepath.filePaths[0] + ' is not a backup file')
+                        }
+                    }
+                })
+            }
+        }).catch((err) => {
+            alert('An error occured, ', err)
+        })
+    },
+    selectlocation: async function () {//select location for configuration storage
+        console.log('Select config location')
+        if (config.baseconfig.alt_location != undefined) {
+            var path = dialog.showOpenDialog({ properties: ['createDirectory', 'openDirectory'], defaultPath: config.baseconfig.alt_location.toString() })
+        } else {
+            var path = dialog.showOpenDialog({ properties: ['createDirectory', 'openDirectory'], defaultPath: null })
+        }
+
+        await path.then((path) => {
+            if (path.canceled == true) {//user canceled dialogue
+                config.usedefault()
+            } else {
+                console.warn('Alternate configuration path :', path.filePaths[0])
+
+                config.baseconfig.use_alt_storage = true
+                config.baseconfig.alt_location = path.filePaths[0]
+                localStorage.setItem("APPnamecfg_baseconfig", JSON.stringify(config.baseconfig))//save base config
+
+                if (fs.existsSync(config.baseconfig.alt_location.toString() + "/APPnamecfg config.json")) {//config file already exist there
+                    config.load()
+                } else {//no config file exist there
+                    config.save();
+                }
+            }
+        }).catch((err) => {
+            config.usedefault()
+            alert('An error occured ', err.message)
+        })
     },
     usedefault: function () {//use default storage location
         config.baseconfig.use_alt_storage = false
-        localStorage.setItem("TT01_baseconfig", JSON.stringify(config.baseconfig))//save base config
+        localStorage.setItem("APPnamecfg_baseconfig", JSON.stringify(config.baseconfig))//save base config
     },
+}
+
+function window_menu() {//window menu
+    //build menu
+    const menu_body = new Menu()
+    menu_body.append(new MenuItem({ label: 'Restart application', click() { location.reload() } }))
+    menu_body.append(new MenuItem({ type: 'separator' }))
+    menu_body.append(new MenuItem({
+        label: 'Contact developer', click() {
+            shell.openExternal('https://anthonym01.github.io/Portfolio/?contact=me')
+        }
+    }))
+
+    //attach menu to window
+    window.addEventListener('contextmenu', (event) => {//opens menu on auxilery click
+        event.preventDefault()
+        menu_body.popup({ window: require('electron').remote.getCurrentWindow() })//popup menu
+    }, false)
+}
+
+function textboxmenu() {//text box menus
+    //construct meno
+    const text_box_menu = new Menu.buildFromTemplate([
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectAll' },
+        { role: 'seperator' },
+        { role: 'undo' },
+        { role: 'redo' },
+    ])
+
+    //add events to text boxes
+    textbox.addEventListener('contextmenu', (event) => popupmenu, false)
+
+    //Popup the menu in this window
+    function popupmenu(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        text_box_menu.popup({ window: require('electron').remote.getCurrentWindow() })
+    }
 }
 
 let utility = {//Misculanious utilites
     get_url_variables: function (url) {
-        //Yoinked from
-        //https://gomakethings.com/getting-all-query-string-values-from-a-url-with-vanilla-js/
+        //Yoinked from https://gomakethings.com/getting-all-query-string-values-from-a-url-with-vanilla-js/
         var params = {};
         var parser = document.createElement('a');
         parser.href = url;
@@ -192,11 +263,11 @@ let utility = {//Misculanious utilites
         }
         return params;
         //returns Object { "": "undefined" } if empty
-        //Call with var this_url = getParams(window.location.href);
+        //Call with getParams(window.location.href);
     },
     close: function () {//Close the app
         config.save()
-        window.close()
+        main.clossapp()
     },
     clipboard: async function (textpush) {
         copyText.toString()
@@ -208,7 +279,7 @@ let utility = {//Misculanious utilites
         document.execCommand("copy")
         document.body.removeChild(temptxtbox)
     },
-    rand: {
+    rand: {//'Random' numbers
         HEX: function () { return '#' + Math.floor(Math.random() * 16777215).toString(16) /* hex color code */ },
         RGB: function () { return { RED: this.number(255, 0), GREEN: this.number(255, 0), BLUE: this.number(255, 0) } /* object with RGB color code */ },
         HSL: function () { return { HUE: this.number(360, 0), SATURATION: this.number(100, 0) + '%', LIGHTENESS: this.number(100, 1) + '%' }/* HSL color code */ },
--- Electron/main.js
+++ Electron/main.js
@@ -1,57 +1,104 @@
 const electron = require('electron');//includes electron dependency
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, Tray, Menu, dialog } = electron
 
 const path = require('path');//path to necessary files
 const url = require('url');//web dependency
 const windowStateKeeper = require('electron-window-state');//preserves the window state
+//const { createPublicKey } = require('crypto');
 
-var mainWindow;//defines the window as an abject
+let mainWindow = null;//defines the window as an abject
+let tray = null;
 
-app.on('ready',createmainWindow)
+app.on('ready', function () {//App ready to roll
+	createmainWindow()
+	create_tray()
+})
 
-function createmainWindow(){
-	app.allowRendererProcessReuse = true;
-	const { screenwidth, screenheight } = electron.screen.getPrimaryDisplay().workAreaSize //gets screen size and sets it to height and width
+function createmainWindow() {//Creates the main render process
+	app.allowRendererProcessReuse = true;//Allow render processes
+
+	//window manager plugin stuff
+	const { screenwidth, screenheight } = electron.screen.getPrimaryDisplay().workAreaSize //gets screen size
 	let mainWindowState = windowStateKeeper({
 		defaultWidth: screenwidth,
 		defaultHeight: screenheight
 	})
-	mainWindow = new BrowserWindow({
-		x: mainWindowState.x,
-		y: mainWindowState.y,
+
+	mainWindow = new BrowserWindow({//make main window
+		x: mainWindowState.x,//x psoition
+		y: mainWindowState.y,//y position
 		width: mainWindowState.width,
 		height: mainWindowState.height,
-		backgroundColor: '#ffffff',
+		backgroundColor: '#000000',
 		frame: true,
-		center: true,
+		center: true,//center the window
 		alwaysOnTop: false,
-		icon:undefined,
-		title:'Blach app',
-		minWidth:400,
-		webPreferences:{
-			nodeIntegration:true,
+		icon: path.join(__dirname, '/assets/icons/icon.ico'),
+		title: 'Blach app',
+		//titleBarStyle: 'hiddenInset',
+		minWidth: 400,
+		webPreferences: {
+			nodeIntegration: true,
+			enableRemoteModule: true,
 		}
 	})
-	mainWindow.loadURL(url.format({
+
+	mainWindow.loadURL(url.format({//load html into main window
 		pathname: path.join(__dirname, '/BrowserWindows/MainWindow.html'),
 		protocol: 'file:',
 		slashes: true
 	}))
-	mainWindowState.manage(mainWindow);
+
+	mainWindowState.manage(mainWindow);//give window to window manager plugin
 }
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+function create_tray() {//Create tray
+	tray = new Tray('assets/icons/icon.ico')
+
+	tray.addListener('double-click', function () {//double click on tray
+		if (BrowserWindow.getAllWindows().length !== 0) {
+			mainWindow.show()
+		} else {
+			createmainWindow()
+		}
+	})
+
+	const contextMenu = Menu.buildFromTemplate([//build contect memu
+		{
+			id: 'name', label: 'Blach app alpha'
+			, click() {
+				if (BrowserWindow.getAllWindows().length !== 0) {//if no windows
+					mainWindow.show()
+				} else {
+					createmainWindow()
+				}
+			}
+		},
+		{ type: 'separator' },//seperator
+		{ role: 'Quit' },//quit app
+	])
+	tray.setToolTip('Blach app')
+	tray.setContextMenu(contextMenu)
+}
+
+app.on('window-all-closed', () => {//all windows closed
+	if (process.platform !== 'darwin' && tray == null) {
+		app.quit();
+	}
 })
 
 app.on('activate', () => {//for darwin
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createmainWindow();
-  }
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createmainWindow();
+	} else {
+		mainWindow.show();
+	}
 });
 
-exports.clossapp = () =>{
-	app.quit();
-}
+exports.clossapp = () => { app.quit() }//export quit app
+
+exports.minimize = () => { mainWindow.minimize() }//minimize window
+
+exports.setontop = () => { mainWindow.setAlwaysOnTop(true) }//always on top the window
+
+exports.setnotontop = () => { mainWindow.setAlwaysOnTop(false) }//always on top'nt the window
