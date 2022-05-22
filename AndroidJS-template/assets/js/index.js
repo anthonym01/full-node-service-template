96623265e8e71395c984478c244530c7a4728f26
Samuel A. Matheson <57382365+anthonym01@users.noreply.github.com>
2022-05-21 20:47:24 -0500

    capacitor template

--- AndroidJS-template/assets/js/index.js
+++ AndroidJS-template/assets/js/index.js
@@ -9,3 +9,8 @@ front.on("hello from back", function(msg){
 front.on('fake console',function(msg){
 	console.log('From the back: \n',msg)
 })
+
+window.addEventListener('onkeydown',function(e){
+	e.preventDefault();
+	console.log(e);
+});
--- AndroidJS-template/main.js
+++ AndroidJS-template/main.js
@@ -4,7 +4,6 @@ back.on("hello from front", function(){
 	back.send("hello from back", "Hello from Android JS");
 });
 
-
 function consolelog(msg){// Send error messages to the web view for debugging purposes
 	back.send("fake console",msg);
 }
