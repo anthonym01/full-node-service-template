cda1150c7e1160edbafa28963b8edb25ace9cfee
Samuel A. Matheson <samuelmatheson15@gmail.com>
2024-07-11 13:18:31 -0500

    intriguing

--- express-server template/tests/database.js
+++ express-server template/tests/database.js
@@ -1,6 +1,6 @@
 const database = require('../database');
 
-database.initalize();//initalize database
-database.get_user_data_by_uuid(1717209024517);//test get user data
+database.initalize('chickenn time');//initalize database
+database.get_user_data_by_uuid(1720721750676);//test get user data
 database.create_user({ uname: "testduplicate", password: "0000" });//test create user duplication
 database.get_user_data_by_username("testduplicate");//test get user data by username
